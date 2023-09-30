74b96f5ef8f67517b01ace869ff933cd036e5168
NellyOgo <nellyogorodnik@gmail.com>
2023-10-01 00:38:15 +0300

    try5 Nell

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -33,7 +33,7 @@ export function Form({ onData }) {
           type="text"
           name="name"
           value={name}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+         pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}" 
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           placeholder=" enter name"
