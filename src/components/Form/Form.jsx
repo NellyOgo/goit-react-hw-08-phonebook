d63840426c32d45778d887f2d274c93e2e3f8023
NellyOgo <nellyogorodnik@gmail.com>
2023-10-01 13:35:50 +0300

    try6 Nell

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -33,7 +33,7 @@ export function Form({ onData }) {
           type="text"
           name="name"
           value={name}
-         pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}" 
+         pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$" 
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           placeholder=" enter name"
@@ -46,7 +46,7 @@ export function Form({ onData }) {
           type="tel"
           name="number"
           value={number}
-          pattern="[\+]\d{3}\s[\(]\d{2}[\)]\s\d{3}[\-]\d{2}[\-]\d{2}"
+          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           placeholder=" enter number"
